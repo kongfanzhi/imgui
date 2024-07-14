cae86c303e405908fb71df65aa5c1e243ccfaa7f
kongfanzhi <463120197@qq.com>
2024-07-15 01:10:50 +0800

    Set OpenGL

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,21 +23,12 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
+		staticruntime "on"
 
 	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
